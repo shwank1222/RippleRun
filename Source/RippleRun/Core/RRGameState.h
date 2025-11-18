a474d9188b70c19a90bc5a194b52dd790841914e
ttingpan <dx123xb@naver.com>
2025-11-18 14:24:59 +0900

    [Add] Added Default Framework Classes
    - 게임 모드, 게임 스테이트, 플레이어 스테이트 클래스 생성

---
+++ Source/RippleRun/Core/RRGameState.h
@@ -0,0 +1,13 @@
+﻿// Copyright TeamName. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "RRGameState.generated.h"
+
+UCLASS()
+class RIPPLERUN_API ARRGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+};
