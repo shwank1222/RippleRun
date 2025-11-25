409b823252e2dd5fb9e36195904bd9eb303ccbab
ttingpan <dx123xb@naver.com>
2025-11-25 18:15:31 +0900

    [Add] Introduced Replay Subsystem and Updated Core Features
    - 새로운 리플레이 서브시스템(RRReplaySubsystem) 추가: 기록 시작, 정지 및 재생 기능 구현
    - `ARRGameState`에 `ReplayTargetStone` 변수 및 `GetLifetimeReplicatedProps` 함수 추가
    - 블루프린트(BP_ReplayCamera, WBP_ReplayWidget 등) 및 BP_RRPlayerController 관련 변경
    - 주요 UI 및 리플레이 관련 연동 로직 업데이트

---
+++ Source/RippleRun/Core/RRReplaySubsystem.cpp
@@ -0,0 +1,78 @@
+﻿// Copyright TeamName. All Rights Reserved.
+
+
+#include "RRReplaySubsystem.h"
+
+#include "RRGameState.h"
+#include "Gameplay/Stone/SkippingStone.h"
+#include "Kismet/GameplayStatics.h"
+#include "Net/UnrealNetwork.h"
+
+static const FString ReplayName = TEXT("ReplayTest");
+
+void URRReplaySubsystem::StartRecordingReplay()
+{
+	UE_LOG(LogTemp, Warning, TEXT("===== Start recording replay! ====="));
+	GetGameInstance()->StartRecordingReplay(ReplayName, FString());
+}
+
+void URRReplaySubsystem::StopRecordingReplay()
+{
+	GetGameInstance()->StopRecordingReplay();
+	UE_LOG(LogTemp, Warning, TEXT("===== Stop recording replay! ====="));
+}
+
+void URRReplaySubsystem::PlayReplay()
+{
+	UE_LOG(LogTemp, Warning, TEXT("===== Play replay! ====="));
+
+	FNetworkReplayDelegates::OnReplayPlaybackComplete.AddUObject(this, &ThisClass::OnReplayPlaybackComplete);
+
+	GetGameInstance()->PlayReplay(ReplayName);
+
+	FTimerHandle TimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ThisClass::ConnectReplayCamera, 0.5f, false);
+}
+
+void URRReplaySubsystem::ConnectReplayCamera()
+{
+	TArray<AActor*> Actors;
+	UGameplayStatics::GetAllActorsOfClassWithTag(GetWorld(), AActor::StaticClass(), TEXT("ReplayCamera"), Actors);
+
+	if (Actors.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("===== No replay camera found! ====="));
+		return;
+	}
+
+	if (APlayerController* PC = GetWorld()->GetFirstPlayerController())
+	{
+		PC->SetViewTargetWithBlend(Actors[0]);
+	}
+
+	GetWorld()->GetTimerManager().SetTimer(TestTimerHandle, this, &ThisClass::TestLog, 0.1f, true);
+}
+
+void URRReplaySubsystem::TestLog()
+{
+	UE_LOG(LogTemp, Warning, TEXT("===== Test Log! ====="));
+
+	if (ARRGameState* GS = GetWorld()->GetGameState<ARRGameState>())
+	{
+		if (IsValid(GS->ReplayTargetStone))
+		{
+			// UE_LOG(LogTemp, Warning, TEXT("===== Test Value: %.2f ====="), GS->ReplayTargetStone->TestValue);
+		}
+	}
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void URRReplaySubsystem::OnReplayPlaybackComplete(UWorld* World)
+{
+	UE_LOG(LogTemp, Warning, TEXT("===== Replay completed! ====="));
+	FNetworkReplayDelegates::OnReplayPlaybackComplete.RemoveAll(this);
+
+	OnReplayCompleted.Broadcast();
+
+	GetWorld()->GetTimerManager().ClearTimer(TestTimerHandle);
+}
---
+++ Source/RippleRun/Core/RRReplaySubsystem.h
@@ -0,0 +1,39 @@
+﻿// Copyright TeamName. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "RRReplaySubsystem.generated.h"
+
+class ASkippingStone;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnReplayCompleted);
+
+UCLASS()
+class RIPPLERUN_API URRReplaySubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Replay")
+	void StartRecordingReplay();
+	UFUNCTION(BlueprintCallable, Category = "Replay")
+	void StopRecordingReplay();
+	UFUNCTION(BlueprintCallable, Category = "Replay")
+	void PlayReplay();
+	
+	UPROPERTY(BlueprintAssignable, Category = "Replay")
+	FOnReplayCompleted OnReplayCompleted;
+	
+private:
+	UFUNCTION()
+	void OnReplayPlaybackComplete(UWorld* World);
+	
+	UFUNCTION()
+	void ConnectReplayCamera();
+	
+	UFUNCTION()
+	void TestLog();
+	
+	FTimerHandle TestTimerHandle;
+};
