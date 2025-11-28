1915c283d71e7b2e722164b7a93376a101e8d078
ttingpan <dx123xb@naver.com>
2025-11-28 18:30:41 +0900

    [Update] UI Design
    - UI 디자인 수정
    - 자잘한 버그 수정

--- Source/RippleRun/Core/RRGameUserSettings.h
+++ Source/RippleRun/Core/RRGameUserSettings.h
@@ -15,14 +15,14 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Audio")
 	FORCEINLINE void SetMasterVolume(const float Volume) { MasterVolume = FMath::Clamp(Volume, 0.0f, 1.0f); }
 	UFUNCTION(BlueprintCallable, Category = "Audio")
-	FORCEINLINE void SetMusicVolume(const float Volume) { MusicVolume = FMath::Clamp(Volume, 0.0f, 1.0f); }
+	FORCEINLINE void SetAmbienceVolume(const float Volume) { AmbienceVolume = FMath::Clamp(Volume, 0.0f, 1.0f); }
 	UFUNCTION(BlueprintCallable, Category = "Audio")
 	FORCEINLINE void SetSFXVolume(const float Volume) { SFXVolume = FMath::Clamp(Volume, 0.0f, 1.0f); }
 	
 	UFUNCTION(BlueprintPure, Category = "Audio")
 	FORCEINLINE float GetMasterVolume() const { return MasterVolume; }
 	UFUNCTION(BlueprintPure, Category = "Audio")
-	FORCEINLINE float GetMusicVolume() const { return MusicVolume; }
+	FORCEINLINE float GetAmbienceVolume() const { return AmbienceVolume; }
 	UFUNCTION(BlueprintPure, Category = "Audio")
 	FORCEINLINE float GetSFXVolume() const { return SFXVolume; }
 	
@@ -30,7 +30,7 @@ private:
 	UPROPERTY(Config)
 	float MasterVolume = 1.0f;
 	UPROPERTY(Config)
-	float MusicVolume = 1.0f;
+	float AmbienceVolume = 1.0f;
 	UPROPERTY(Config)
 	float SFXVolume = 1.0f;
 };
